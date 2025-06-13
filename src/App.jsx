11f0e11cbac5cddd4c760a14dd6ea062676e3eae
nikhithareddy45 <nikkireddyperugu@gmail.com>
2025-06-13 23:24:12 +0530

    hash-router

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Add other routes here as needed */}
       </Routes>
       <Footer />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import  store  from '../store.js'
-import React from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import {BrowserRouter} from 'react-router-dom'
+// // import { Provider } from 'react-redux'
+// // import  store  from '../store.js'
+// import React from 'react'
+
+// createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//   {/* <Provider store ={store}> */}
+//     <App />
+//     {/* </Provider> */}
+//   </BrowserRouter>,
+// )
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  {/* <Provider store ={store}> */}
+  <HashRouter>
     <App />
-    {/* </Provider> */}
-  </BrowserRouter>,
-)
+  </HashRouter>
+);
